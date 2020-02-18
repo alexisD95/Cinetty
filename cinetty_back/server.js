2c028cea48e982ff9760d4c77b41f95d3c4e2903
Gabriel <gabriel.ruaud17@gmail.com>
2020-02-18 14:32:19 +0100

    Version avec début du front

--- cinetty_back/server.js
+++ cinetty_back/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +15,7 @@ app.use(express.json());
 // Connect to MongoDB
 const uri = process.env.MONGO_ATLAS_URI;
 mongoose.connect(uri, {
+  useCreateIndex : true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
@@ -23,9 +24,15 @@ mongoose.connect(uri, {
 // Connection approval
 mongoose.connection.on('connected', function(){
     console.log("MongoDB database connection established successfully");
-    console.log(`Browse : http://localhost:${port}`);
 });
 
+//Routeur and routes initialization
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+//Port listening
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 })
