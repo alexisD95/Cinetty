001b554f4d7cc1c3870af6f5fc2fe6ad0fd5f649
Alexis Direz <alex.direz@gmail.com>
2020-02-19 10:56:47 +0100

    get arguments

--- cinetty_front/src/App.js
+++ cinetty_front/src/App.js
@@ -2,7 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Modele simple du lien https://www.cinetty.com/id?021564
+
+//NB: Pour indenter taper shift + alt + F
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    var number;
+
+    //ajouter les fonctions crées:
+    // this.nom_fonction = this.nom_fonction.bind(this);
+    this.getIDPoubelle = this.getIDPoubelle.bind(this);
+    this.updatePoubelle = this.updatePoubelle.bind(this);
+
+  }
+
+  getIDPoubelle() {
+    //utiliser le path pour trouver l'id de la poubelle scannée avec son QR code
+
+    const url = require('url')
+    const qs = require('querystring')
+
+    const serverHandle = function (req, res) {
+      const route = url.parse(req.url)
+      const path = route.pathname
+      const params = qs.parse(route.query)
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+      if (path === '/id' && 'number' in params) {
+        res.write('You are connected to trash n° ' + params['number'])
+      } else {
+        res.write('Try to scan a QR')
+      }
+
+      res.end();
+    }
+
+    console.log('number');
+  }
+
+  updatePoubelle(number) {
+    //utilisé pour mettre à jour les infos de la poubelle
+
+  }
+
   render() {
     return (
       <div className="App">
